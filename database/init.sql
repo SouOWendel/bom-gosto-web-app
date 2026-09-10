8bd11d48b6682dfdf2d2a69c3619fbea2983ec6e
SouOWendel <souowendel@gmail.com>
2026-09-10 16:52:42 -0300

    feat(database): adiciona script de inicialização do PostgreSQL
    Inclui a extensão "uuid-ossp" para uso no banco de dados da Confeitaria Bom Gosto.

---
+++ database/init.sql
@@ -0,0 +1,4 @@
+-- Script de inicialização do PostgreSQL para a Confeitaria Bom Gosto
+-- Executado automaticamente na criação inicial do container do banco de dados
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
